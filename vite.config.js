026d903464995c381da1b33472509c8e7d8c0c11
code-biz <honeybiz@gmail.com>
2025-05-27 14:11:09 +0500

    pushhed

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/PlantShoppingCart_Redux",
+  base: "/plant-shopping-cart_redux",
   plugins: [react()],
 })
