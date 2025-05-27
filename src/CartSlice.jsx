af8485871e350c578a838db24207d01f5c240949
code-biz <honeybiz@gmail.com>
2025-05-27 13:36:56 +0500

    react-redux-project

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,19 +1,44 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 export const CartSlice = createSlice({
-  name: 'cart',
+  name: "cart",
   initialState: {
     items: [], // Initialize items as an empty array
   },
   reducers: {
     addItem: (state, action) => {
-    
-    },
-    removeItem: (state, action) => {
-    },
-    updateQuantity: (state, action) => {
+      const plant = action.payload;
+      //ANOTHER WAY IS :   const { name, image, cost } = action.payload;
 
-    
+      const itemsArray = state.items;
+
+      const alreadyPresent = itemsArray.find(
+        (item) => item.name === plant.name
+      );
+      if (alreadyPresent) {
+        alreadyPresent.quantity++;
+      } else {
+        itemsArray.push({ ...plant, quantity: 1 });
+        ///ANOTHER WAY IS :     state.items.push({ name, image, cost, quantity: 1 });
+      }
+    },
+
+    removeItem: (state, action) => {
+      let itemsArray = state.items;
+      const plant = action.payload; // THE ACTION PAYLOAD CONTAINS PLANT OBJECT
+      itemsArray = itemsArray.filter((item) => item.name !== plant.name);
+      state.items = itemsArray;
+    },
+
+    updateQuantity: (state, action) => {
+      const itemsArray = state.items;
+      const plant = action.payload;
+      const alreadyPresent = itemsArray.find(
+        (item) => item.name === plant.name
+      );
+      if (alreadyPresent) {
+        alreadyPresent.quantity = plant.quantity;
+      }
     },
   },
 });
